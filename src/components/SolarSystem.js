39c0acd91a3e42f014dd96212bc2d708b126fa89
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-04-07 13:23:40 -0300

    requisito 4 feito

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         SolarSystem
+        <Title headline="Planetas" />
       </div>
     );
   }
