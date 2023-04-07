1520fee2cedc4343103c7424361bdeda4b7ac25d
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-04-07 13:13:47 -0300

    requisito 2 feito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header.js';
+import SolarSystem from './components/SolarSystem.js';
 
 class App extends React.Component {
   render() {
     return (
       <main>
         <Header />
+        <SolarSystem />
       </main>
     );
   }
